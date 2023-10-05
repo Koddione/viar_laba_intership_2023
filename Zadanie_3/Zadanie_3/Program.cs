f50fc0ee9fd4a37c3309d615c33e9c946531778f
Koddione <135285665+Koddione@users.noreply.github.com>
2023-10-05 23:40:35 +0300

    Задание 3
    С клавиатуры вводится число, вычислить его факториал.

---
+++ Zadanie_3/Zadanie_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(factorial(num));
+            Console.ReadKey();
+        }
+        static int factorial(int num)
+        {
+            int resh = 1;
+            for (int i = 1; i < num+1; i++)
+            {
+                resh *= i;
+            }
+            return resh;
+        }
+    }
+}

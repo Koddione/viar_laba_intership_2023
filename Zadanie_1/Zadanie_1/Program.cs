57ae5881f9f4296a8f0b5c8436bb214c38455c35
Koddione <135285665+Koddione@users.noreply.github.com>
2023-10-05 23:18:08 +0300

    Задание 1
    С клавиатуры вводятся 3 числа. Вывести их в порядке убывания и возрастания (Например: 4, 7, 2 → 2, 4, 7 и 4, 7, 2 → 7, 4, 2).

---
+++ Zadanie_1/Zadanie_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int one = Convert.ToInt32(Console.ReadLine());
+        int two = Convert.ToInt32(Console.ReadLine());
+        int three = Convert.ToInt32(Console.ReadLine());
+        int Max(int one,int two)=>one>two?one:two;
+        int Min(int one, int two) => one > two ? two : one;
+        int Max3(int one,int two,int three)=>three>Max(one,two)?three:Max(one,two);
+        int Min3(int one, int two,int three) => three<Min(one,two)?three:Min(one,two);
+        Console.WriteLine(Min3(one, two, three)+ " " +((one + two + three) - (Max3(one,two,three)+Min3(one,two,three))) + " " + Max3(one,two,three));
+
+    }
+}

ae22b7d3110fca3e284909566790fc00feffe2a1
Koddione <135285665+Koddione@users.noreply.github.com>
2023-10-08 20:40:36 +0300

    Задание 7
    Дан двумерный массив поменять местами элементы, расположенные на главной диагонали с противоположной диагонально.

---
+++ Zadanie_7/Zadanie_7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zadanie_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int n = rnd.Next(3, 10);
+            int[,] mass = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    mass[i, j] = rnd.Next(10, 50);
+                    Console.Write(mass[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                int temp = mass[i, i];
+                mass[i, i] = mass[i, n - 1 - i];
+                mass[i, n - 1 - i] = temp;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(mass[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
